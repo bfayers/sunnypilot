6b6c859a8982ee8d21bc37df7c5f3a6cd06376e6
bfayers <bfayers@users.noreply.github.com>
2026-07-25 15:25:53 +0100

    longitudinal planner: clean up leftover force_slow_decel check

--- openpilot/selfdrive/controls/lib/longitudinal_planner.py
+++ openpilot/selfdrive/controls/lib/longitudinal_planner.py
@@ -144,9 +144,6 @@ class LongitudinalPlanner(LongitudinalPlannerSP):
     # Get new v_cruise and a_desired from Smart Cruise Control and Speed Limit Assist
     v_cruise, self.a_desired = LongitudinalPlannerSP.update_targets(self, sm, self.v_desired_filter.x, self.a_desired, v_cruise)
 
-    if force_slow_decel:
-      v_cruise = 0.0
-
     self.mpc.set_weights(prev_accel_constraint, personality=sm['selfdriveState'].personality)
     self.mpc.set_cur_state(self.v_desired_filter.x, self.a_desired)
     self.mpc.update(sm['radarState'], personality=sm['selfdriveState'].personality)
